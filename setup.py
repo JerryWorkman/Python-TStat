098eb462d5da627db441533349bb6398a3b11b99
Jerry Workman <jerry.workman@gmail.com>
2016-01-24 22:30:46 -0500

    Added to allow installation to system / site python library

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='radiotstat',
+      version='1.0',
+      description='Python interface for Radio Thermostat wifi-enabled thermostats.',
+      author='Paul Jennings',
+      author_email='pjennings-tstat@pjennings.net',
+      url='https://github.com/pjennings/Python-TStat',
+      packages=['radiotstat'],
+     )
